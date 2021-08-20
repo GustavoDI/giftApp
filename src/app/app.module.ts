a1c4b492a17b30eb2364d73ffa7c2964ff037ca9
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2021-08-20 15:28:55 -0400

    cambio en las import por ngmodule duplicate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    SidebarComponent
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
