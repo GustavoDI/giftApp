2d7ea2a75f2ff3835b61122edfbef869b14b139d
Gustavo Diaz <gustavo.ianiszewski@gmail.com>
2021-08-24 18:06:01 -0400

    Buscar y mostrar en side bar

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -12,6 +12,7 @@ export class BusquedaComponent {
   
   @ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
 
+  // injectar el servicio 
   constructor(private gifsService: GifsService) { }
   
   buscar(){
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,18 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent  {
 
-  constructor() { }
+  // En este punto estamos injectando el servicio.
+  constructor(private gifsServicio: GifsService) { }
 
-  ngOnInit(): void {
+  get historial(){
+    return this.gifsServicio.historial;
   }
-
+  
 }
